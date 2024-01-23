4d56734ce963b534106b0d833977edf3ebe8460b
VirxEC <virx@virxcase.dev>
2024-01-22 19:47:08 -0500

    Decrypt message two via swap & reverse

--- Git_Github_HW/decrypt_message_two.py
+++ Git_Github_HW/decrypt_message_two.py
@@ -5,3 +5,23 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+
+decrypted_message = encrypted_message + ""
+
+start = 0
+end = len(encrypted_message) - 1
+
+while start < end:
+    # swap the characters at the start and end indices
+    decrypted_message = (
+        decrypted_message[:start]
+        + encrypted_message[end]
+        + decrypted_message[start + 1 : end]
+        + encrypted_message[start]
+        + decrypted_message[end + 1 :]
+    )
+    start += 2
+    end -= 2
+
+decrypted_message = decrypted_message[::-1]
+print(decrypted_message)
